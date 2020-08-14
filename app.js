07836a1ff189c2d83d6e6caf268af61a3d42b7b9
Joseph Fletcher <jwpf100@gmail.com>
2020-08-14 14:19:45 +0100

    Webpack original config works.  Next step to get this working on heroku, then add pics, then split dev and production

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //Using this to include the bootstrap js modules - to be replaced with webpack
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+//app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);

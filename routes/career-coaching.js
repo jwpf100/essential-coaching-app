8e8f7588b78d08511c9c432d96979553c7bb6947
Joseph Fletcher <jwpf100@gmail.com>
2020-09-09 17:06:15 +0100

    Added what I do page

--- routes/career-coaching.js
+++ routes/career-coaching.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET aboutme page. */
 router.get('/', function(req, res, next) {
-  res.render('career-coaching', { title: 'Essential Coaching - Career Coaching' });
+  res.render('career-coaching', { title: 'Essential Coaching Process - Essential Coaching',
+  metaDescription: "What is career coaching and how can it help you?"
+});
 });
 
 module.exports = router;
